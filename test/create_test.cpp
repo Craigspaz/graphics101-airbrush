c4177477d86ea40fefb292f6f80304c51d7a6364
Yotam Gingold <yotam@yotamgingold.com>
2022-08-16 01:14:00 -0400

    tests are working

--- test/create_test.cpp
+++ test/create_test.cpp
@@ -4,7 +4,7 @@
 #include <cmath> // std::abs
 #include "airbrush.h"
 
-// #include <iostream>
+#include <iostream>
 
 namespace graphics101 {
 bool operator==( const ColorRGBA8& lhs, const ColorRGBA8& rhs ) {
@@ -28,7 +28,7 @@ using namespace graphics101;
 
 TEST_CASE( "Create Airbrush Constant Test" ) {
     const ColorRGBA8 color( 100, 150, 200, 30 );
-    const int radius = 7;
+    const int radius = 50;
     Image airbrush_image;
     
     create_airbrush( airbrush_image, AirBrushShape::Constant, radius, color );
@@ -44,12 +44,12 @@ TEST_CASE( "Create Airbrush Constant Test" ) {
     CHECK( airbrush_image.pixel( radius, radius ) == color );
     
     // Another point inside should be color.
-    CHECK( airbrush_image.pixel( radius/2, radius ) == color );
+    CHECK( airbrush_image.pixel( radius/5, radius ) == color );
 }
 
 TEST_CASE( "Create Airbrush Linear Test" ) {
     const ColorRGBA8 color( 100, 150, 200, 30 );
-    const int radius = 7;
+    const int radius = 50;
     Image airbrush_image;
     
     create_airbrush( airbrush_image, AirBrushShape::Linear, radius, color );
@@ -67,5 +67,32 @@ TEST_CASE( "Create Airbrush Linear Test" ) {
     // Another point inside should have color's RGB.
     CHECK( SameRGBIgnoreA( airbrush_image.pixel( radius/2, radius ), color ) );
     // Alpha should be scaled down.
-    CHECK( std::abs( airbrush_image.pixel( radius/2, radius ).a - color.a/2 ) < 2 );
+    // std::cerr << "airbrush_image.pixel( radius/5, radius ).a: " << int(airbrush_image.pixel( radius/5, radius ).a) << std::endl;
+    // 5 is 2% of the range of values.
+    CHECK( std::abs( airbrush_image.pixel( radius/5, radius ).a - 6 ) <= 5 );
+}
+
+TEST_CASE( "Create Airbrush Quadratic Test" ) {
+    const ColorRGBA8 color( 100, 150, 200, 30 );
+    const int radius = 50;
+    Image airbrush_image;
+    
+    create_airbrush( airbrush_image, AirBrushShape::Quadratic, radius, color );
+    
+    // Check the dimensions of the output.
+    CHECK( airbrush_image.width() == 2*radius + 1 );
+    CHECK( airbrush_image.height() == 2*radius + 1 );
+    
+    // The corner should be transparent.
+    CHECK( airbrush_image.pixel( 0, 0 ).a == 0 );
+    
+    // The center should be color.
+    CHECK( airbrush_image.pixel( radius, radius ) == color );
+    
+    // Another point inside should have color's RGB.
+    CHECK( SameRGBIgnoreA( airbrush_image.pixel( radius/2, radius ), color ) );
+    // Alpha should be scaled down.
+    // std::cerr << "airbrush_image.pixel( radius/5, radius ).a: " << int(airbrush_image.pixel( radius/5, radius ).a) << std::endl;
+    // 5 is 2% of the range of values.
+    CHECK( std::abs( airbrush_image.pixel( radius/5, radius ).a - 2 ) <= 5 );
 }
