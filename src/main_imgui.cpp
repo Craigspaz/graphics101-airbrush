34c5d9ad268867dee9e8fd10411dd938ec1f7eaa
Yotam Gingold <yotam@yotamgingold.com>
2022-08-26 01:44:37 -0400

    Qt -> Dear ImGui transition is complete

--- src/main_imgui.cpp
+++ src/main_imgui.cpp
@@ -42,7 +42,8 @@ void AirbrushFromState( graphics101::Image& airbrush_image, const BrushState& st
 }
 
 SDL_Texture* CreateTextureForImage( SDL_Renderer* renderer, const graphics101::Image& image ) {
-    SDL_Texture* tex = SDL_CreateTexture( renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, image.width(), image.height() );
+    // TODO: Why does SDL_PIXELFORMAT_ABGR8888 work? Our data is SDL_PIXELFORMAT_RGBA8888.
+    SDL_Texture* tex = SDL_CreateTexture( renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, image.width(), image.height() );
     if( SDL_SetTextureBlendMode( tex, SDL_BLENDMODE_BLEND ) != 0 ) SDL_Log( "Error: %s", SDL_GetError() );
     return tex;
 }
@@ -83,8 +84,8 @@ int main( int argc, const char* argv[] ) {
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
+    // ImGui::StyleColorsDark();
+    ImGui::StyleColorsLight();
 
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForSDLRenderer( window, renderer );
@@ -106,6 +107,13 @@ int main( int argc, const char* argv[] ) {
     //IM_ASSERT(font != NULL);
 
     // Our state
+    int flow_rate = 66;
+    int mouse_last_x(-1), mouse_last_y(-1);
+    Uint64 lastPaintTicks = SDL_GetTicks64() - flow_rate;
+    // Where to save
+    char save_path[2048] = "artwork.png";
+    
+    // The airbrush
     BrushState last_state;
     BrushState new_state;
     graphics101::Image airbrush_image;
@@ -114,8 +122,16 @@ int main( int argc, const char* argv[] ) {
     // Create the texture.
     SDL_Texture* airbrush_tex = CreateTextureForImage( renderer, airbrush_image );
     if( UploadImageToTexture( airbrush_image, airbrush_tex ) != 0 ) SDL_Log( "Error: %s", SDL_GetError() );
-    int flow_rate = 66;
-    bool drawing = false;
+    
+    // The canvas
+    graphics101::Image canvas_image;
+    {
+        int w, h;
+        SDL_GetWindowSize( window, &w, &h );
+        canvas_image.resize( w, h, graphics101::ColorRGBA8( 255, 255, 255, 255 ) );
+    }
+    SDL_Texture* canvas_tex = CreateTextureForImage( renderer, canvas_image );
+    if( UploadImageToTexture( canvas_image, canvas_tex ) != 0 ) SDL_Log( "Error: %s", SDL_GetError() );
     
     // Main loop
     bool done = false;
@@ -128,9 +144,17 @@ int main( int argc, const char* argv[] ) {
         SDL_Event event;
         while( SDL_PollEvent( &event ) ) {
             ImGui_ImplSDL2_ProcessEvent( &event );
-            if( event.type == SDL_QUIT ) done = true;
-            if( event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window) ) done = true;
-            // TODO: Handle resize event
+            if( event.type == SDL_QUIT ) { done = true; }
+            if( event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window) ) { done = true; }
+            // Resize canvas upon resize event.
+            if( event.type == SDL_WINDOWEVENT && event.window.event ==  SDL_WINDOWEVENT_SIZE_CHANGED ) {
+                SDL_Log( "New window size: %d x %d", event.window.data1, event.window.data2 );
+                canvas_image.resize( event.window.data1, event.window.data2, graphics101::ColorRGBA8(255,255,255,255) );
+                // Re-create the texture.
+                SDL_DestroyTexture( canvas_tex );
+                canvas_tex = CreateTextureForImage( renderer, canvas_image );
+                if( UploadImageToTexture( canvas_image, canvas_tex ) != 0 ) SDL_Log( "Error: %s", SDL_GetError() );
+            }
         }
         
         // Start the Dear ImGui frame
@@ -139,8 +163,8 @@ int main( int argc, const char* argv[] ) {
         ImGui::NewFrame();
         
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        static bool show_demo_window = true;
-        ImGui::ShowDemoWindow(&show_demo_window);
+        // static bool show_demo_window = true;
+        // ImGui::ShowDemoWindow(&show_demo_window);
         
         // 2. Our window.
         {
@@ -148,14 +172,61 @@ int main( int argc, const char* argv[] ) {
             
             ImGui::Combo( "Shape", reinterpret_cast<int*>(&new_state.shape), "Constant\0Linear\0Quadratic\0Special\0\0" );
             ImGui::ColorPicker4("Color", (float*)&new_state.color, ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_NoDragDrop );
-            ImGui::SliderInt("Radius", &new_state.radius, 0, 100 );
+            ImGui::SliderInt("Radius", &new_state.radius, 1, 100 );
             ImGui::SliderInt("Flow rate", &flow_rate, 0, 300 );
             ImGui::Image( airbrush_tex, ImVec2(airbrush_image.width(), airbrush_image.height()), ImVec2(0.0f, 0.0f), ImVec2(1.0f, 1.0f), ImVec4(1.0f, 1.0f, 1.0f, 1.0f), ImVec4(1.0f, 1.0f, 1.0f, 0.0f) );
+            
+            if( ImGui::Button("Save") ) {
+                canvas_image.save( save_path );
+                SDL_Log( "Saved image to: %s", save_path );
+            }
+            ImGui::SameLine();
+            ImGui::InputTextWithHint( "##", "artwork.png", save_path, IM_ARRAYSIZE(save_path) );
             
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
         
+        // Rendering
+        ImGui::Render();
+        SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
+        // Clear to white
+        SDL_RenderClear( renderer );
+        
+        // Draw the canvas
+        if( !io.WantCaptureMouse ) {
+            // If the mouse is down and moved or enough time elapsed, deposit paint.
+            int mouse_x, mouse_y;
+            const Uint32 buttons = SDL_GetMouseState( &mouse_x, &mouse_y );
+            Uint64 nowPaintTicks = SDL_GetTicks64();
+            // Draw if the pressed mouse has moved or enough time has passed.
+            if(
+                (buttons & SDL_BUTTON_LMASK) != 0
+                &&
+                ( mouse_x != mouse_last_x || mouse_y != mouse_last_y || nowPaintTicks - lastPaintTicks > flow_rate )
+                ) {
+                // Draw into the canvas.
+                graphics101::Rect dirty = graphics101::paint_at( canvas_image, airbrush_image, mouse_x, mouse_y );
+                // Update the dirty region of the canvas.
+                SDL_Rect dirty_tex = { .x = dirty.x, .y = dirty.y, .w = dirty.width, .h = dirty.height };
+                SDL_UpdateTexture( canvas_tex, &dirty_tex, canvas_image.scanline(dirty.y) + dirty.x, sizeof(graphics101::ColorRGBA8)*( canvas_image.scanline(1)-canvas_image.scanline(0) ) );
+                
+                // Update our mouse tracking state.
+                mouse_last_x = mouse_x;
+                mouse_last_y = mouse_y;
+                lastPaintTicks = nowPaintTicks;
+            }
+        }
+        
+        // Draw the canvas.
+        SDL_RenderCopy( renderer, canvas_tex, nullptr, nullptr );
+        
+        // Draw ImGui
+        ImGui_ImplSDLRenderer_RenderDrawData( ImGui::GetDrawData() );
+        // Swap buffers
+        SDL_RenderPresent( renderer );
+        
+        // Update the airbrush image after we finish drawing, since we might destroy the airbrush_tex we passed to ImGui.
         if(!(last_state == new_state)) {
             // SDL_Log( "Updating airbrush." );
             // Re-create the airbrush image.
@@ -169,34 +240,20 @@ int main( int argc, const char* argv[] ) {
             // Upload the texture data.
             last_state = new_state;
         }
-        
-        // Rendering
-        ImGui::Render();
-        SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-        // Clear to white
-        SDL_RenderClear( renderer );
-        
-        // Draw the canvas
-        if( !io.WantCaptureMouse ) {
-            // If the mouse is down and moved or enough time elapsed, deposit paint.
-        }
-        
-        SDL_RenderCopy( renderer, airbrush_tex, nullptr, nullptr );
-        
-        // Draw ImGui
-        ImGui_ImplSDLRenderer_RenderDrawData( ImGui::GetDrawData() );
-        // Swap buffers
-        SDL_RenderPresent( renderer );
     }
-
+    
+    // Save the image
+    canvas_image.save( "last_canvas.png" );
+    SDL_Log( "Saved image to: %s", "last_canvas.png" );
+    
     // Cleanup
     ImGui_ImplSDLRenderer_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
-
+    
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
+    
     return 0;
 }
